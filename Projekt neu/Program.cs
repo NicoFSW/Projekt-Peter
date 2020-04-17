121d6b8bc6b1c350009d7603028b273f2fa4c7d8
Ramhapp Nico <nico.ramhapp@fsw.at>
2020-04-17 13:35:27 +0200

    Add project files.

---
+++ Projekt neu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Projekt_neu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("hallo");
+
+
+
+        }
+    }
+}
